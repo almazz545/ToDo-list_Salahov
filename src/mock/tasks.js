255e05fb0ac36e64b9e0ce86cab15eac37f9ad35
almazz545 <almaz.sala@mail.ru>
2024-10-23 09:55:57 +0300

    Add files via upload

---
+++ src/mock/tasks.js
@@ -0,0 +1,47 @@
+export const tasks = [
+  {
+    id: "1",
+    title: "Название задачи",
+    status: "backlog",
+  },
+  {
+    id: "2",
+    title: "Название задачи",
+    status: "inProgress",
+  },
+  {
+    id: "3",
+    title: "Название задачи",
+    status: "inProgress",
+  },
+  {
+    id: "4",
+    title: "Название задачи",
+    status: "backlog",
+  },
+  {
+    id: "5",
+    title: "Название задачи",
+    status: "trash",
+  },
+  {
+    id: "6",
+    title: "Название задачи",
+    status: "backlog",
+  },
+  {
+    id: "7",
+    title: "Название задачи",
+    status: "trash",
+  },
+  {
+    id: "8",
+    title: "Название задачи",
+    status: "inProgress",
+  },
+  {
+    id: "9",
+    title: "Название задачи",
+    status: "trash",
+  },
+];
---
+++ src/presenter/tasks-board-presenter.js
@@ -0,0 +1,93 @@
+import TaskColumnComponent from "../view/task-column-component.js";
+import TaskPresenter from "./task-presenter.js";
+import ClearButtonComponent from "../view/clear-button-component.js";
+import { render } from "../framework/render.js";
+import { Status } from "../consts.js";
+
+export default class TasksBoardPresenter {
+  #taskBoardContainer;
+  #tasksModel;
+
+  constructor({ taskBoardContainer, tasksModel }) {
+    this.#taskBoardContainer = taskBoardContainer;
+    this.#tasksModel = tasksModel;
+    this.#tasksModel.addObserver(this.#handleModelChange.bind(this));
+  }
+
+  #handleModelChange() {
+    this.#clearBoard();
+    this.init();
+  }
+
+  #clearBoard() {
+    this.#taskBoardContainer.innerHTML = "";
+  }
+
+  init() {
+    const tasks = [...this.#tasksModel.tasks];
+
+    Object.values(Status).forEach((status) => {
+      var taskColumnElement = this.#renderTasksColumn(
+        status,
+        this.#taskBoardContainer
+      );
+
+      const tasksInCurrentStatus = this.#tasksModel.getTasksByStatus(status);
+
+      const taskPresenter = new TaskPresenter({ tasks: tasksInCurrentStatus });
+      taskPresenter.init(taskColumnElement);
+    });
+
+    this.#makeClearButton();
+  }
+
+  #renderTasksColumn(status, container) {
+    const taskColumnComponent = new TaskColumnComponent({
+      status,
+      onTaskDrop: this.#handleTaskDrop.bind(this),
+    });
+
+    render(taskColumnComponent, container);
+
+    return taskColumnComponent.element;
+  }
+
+  #handleTaskDrop(taskId, newStatus, position) {
+    this.#tasksModel.updateTaskStatus(taskId, newStatus);
+
+    if (position) {
+      this.#tasksModel.moveTask(taskId, position);
+    } else {
+      this.#tasksModel.addTaskToEnd(taskId);
+    }
+  }
+
+  #makeClearButton() {
+    const tasksInStatusTrash = this.#tasksModel.getTasksByStatus(Status.TRASH);
+    const noTasks = tasksInStatusTrash.length === 0;
+
+    const trashContainer = document.querySelector(`.${Status.TRASH}`);
+    const clearButtonComponent = new ClearButtonComponent(
+      noTasks,
+      this.cleanTrash.bind(this)
+    );
+
+    render(clearButtonComponent, trashContainer);
+  }
+
+  createTask() {
+    const taskTitle = document.querySelector("#add-task").value.trim();
+
+    if (!taskTitle) {
+      return;
+    }
+    this.#tasksModel.addTask(taskTitle);
+
+    document.querySelector("#add-task").value = "";
+  }
+
+  cleanTrash() {
+    const tasksInStatusTrash = this.#tasksModel.getTasksByStatus(Status.TRASH);
+    this.#tasksModel.deleteTasks(tasksInStatusTrash);
+  }
+}
